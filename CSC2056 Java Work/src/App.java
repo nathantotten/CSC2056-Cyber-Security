b60ba0c88dbc3291b9558019f09fc7faa5f156d5
nathantotten <74629300+nathantotten@users.noreply.github.com>
2022-02-02 21:17:31 +0000

    Testing the cipher

--- CSC2056 Java Work/src/App.java
+++ CSC2056 Java Work/src/App.java
@@ -3,7 +3,9 @@ public class App {
     public static void main(String[] args) throws Exception {
         System.out.println("Hello, World!");
         CaesarCipher myCipher = new CaesarCipher();
-        String encrypted = myCipher.cipher("Nathan", 4);
-        System.out.println(encrypted);
+        String encrypted1 = myCipher.cipher("Nathan", 4);
+        String encrypted2 = myCipher.cipher("Odhran", 2);
+        System.out.println(encrypted1);
+        System.out.println(encrypted2);
     }
 }
