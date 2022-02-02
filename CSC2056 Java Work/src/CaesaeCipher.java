de10ac5b6512dd5a5e9f890df593d60bce0d8747
nathantotten <74629300+nathantotten@users.noreply.github.com>
2022-02-02 21:03:46 +0000

    Caesar cipher!

---
+++ CSC2056 Java Work/src/CaesaeCipher.java
@@ -0,0 +1,15 @@
+
+public class CaesaeCipher {
+    String cipher(String msg, int shift) {
+        String s = "";
+        int len = msg.length();
+        for (int x = 0; x < len; x++) {
+            char c = (char) (msg.charAt(x) + shift);
+            if (c > 'z')
+                s += (char) (msg.charAt(x) - (26 - shift));
+            else
+                s += (char) (msg.charAt(x) + shift);
+        }
+        return s;
+    }
+}
